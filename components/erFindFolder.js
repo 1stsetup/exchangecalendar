5b67150e9b7e13d7d7fb28d3671e375aa50f4965
Michel Verbraak <info@1st-setup.nl>
2012-09-06 14:12:04 +0200

    Converted erFindFolder to xml2jxon
    Signed-off-by: Michel Verbraak <info@1st-setup.nl>

--- components/erFindFolder.js
+++ components/erFindFolder.js
@@ -88,12 +88,17 @@ erFindFolderRequest.prototype = {
 	{
 //		exchWebService.commonFunctions.LOG("erFindFolderRequest.execute\n");
 
-		var req = <nsMessages:FindFolder xmlns:nsMessages={nsMessages} xmlns:nsTypes={nsTypes}/>;
-		req.@Traversal = "Shallow";
+		var req = exchWebService.commonFunctions.xmlToJxon('<nsMessages:FindFolder xmlns:nsMessages="'+nsMessagesStr+'" xmlns:nsTypes="'+nsTypesStr+'"/>');
+		req.setAttribute("Traversal", "Shallow");
 
-		req.nsMessages::FolderShape.nsTypes::BaseShape = "AllProperties";
+		req.addChildTag("FolderShape", "nsMessages", null).addChildTag("BaseShape", "nsTypes", "AllProperties"); 
 
-		var restr = 
+		var restr = exchWebService.commonFunctions.xmlToJxon('<nsMessages:Restriction xmlns:nsMessages="'+nsMessagesStr+'" xmlns:nsTypes="'+nsTypesStr+'"/>');
+		var isEqualTo = restr.addChildTag("IsEqualTo", "nsTypes", null);
+		isEqualTo.addChildTag("FieldURI", "nsTypes", null).setAttribute("FieldURI", "folder:DisplayName");
+		isEqualTo.addChildTag("FieldURIOrConstant", "nsTypes", null).addChildTag("Constant", "nsTypes", null).setAttribute("Value", this.folderPath[this.folderCount]);
+
+/*		var restr = 
 			<nsMessages:Restriction xmlns:nsMessages={nsMessages} xmlns:nsTypes={nsTypes}>
 				<nsTypes:IsEqualTo>
 					<nsTypes:FieldURI FieldURI="folder:DisplayName"/>
@@ -101,31 +106,23 @@ erFindFolderRequest.prototype = {
 						<nsTypes:Constant Value={this.folderPath[this.folderCount]}/>
 					</nsTypes:FieldURIOrConstant>
 				</nsTypes:IsEqualTo>
-			</nsMessages:Restriction>;
+			</nsMessages:Restriction>;*/
 
-/*			<nsMessages:Restriction xmlns:nsMessages={nsMessages} xmlns:nsTypes={nsTypes}>
-        			<nsTypes:And>
-					<nsTypes:IsEqualTo>
-						<nsTypes:FieldURI FieldURI="folder:DisplayName"/>
-						<nsTypes:FieldURIOrConstant>
-							<nsTypes:Constant Value={this.folderPath[this.folderCount]}/>
-						</nsTypes:FieldURIOrConstant>
-					</nsTypes:IsEqualTo>
-				</nsTypes:And>
-			</nsMessages:Restriction>;
-*/
-		req.appendChild(restr);
+		req.addChildTagObject(restr);
 	
-		req.nsMessages::ParentFolderIds = makeParentFolderIds("ParentFolderIds", this.argument);
+		var parentFolder = makeParentFolderIds2("ParentFolderIds", this.argument);
+		req.addChildTagObject(parentFolder);
 
-		//exchWebService.commonFunctions.LOG("erFindFolderRequest.execute:"+String(this.parent.makeSoapMessage(req)));
+		this.parent.xml2jxon = true;
+
+		exchWebService.commonFunctions.LOG("erFindFolderRequest.execute:"+String(this.parent.makeSoapMessage(req)));
                 this.parent.sendRequest(this.parent.makeSoapMessage(req), this.serverUrl);
 
 	},
 
 	onSendOk: function _onSendOk(aExchangeRequest, aResp)
 	{
-		//exchWebService.commonFunctions.LOG("erFindFolderRequest.onSendOk:"+String(aResp));
+		exchWebService.commonFunctions.LOG("erFindFolderRequest.onSendOk:"+String(aResp));
 		// Get FolderID and ChangeKey
 		var aContinue = true;
 		var aError = false;
@@ -133,23 +130,19 @@ erFindFolderRequest.prototype = {
 		var aMsg = "";
 		var aResult = undefined;
 
-		try {
-			var totalItemsInView = aResp.nsSoap::Body.nsMessages::FindFolderResponse..nsMessages::RootFolder.@TotalItemsInView;
-		}
-		catch(err) {
-			aMsg = err;
-			aCode = this.parent.ER_ERROR_FINDFOLDER_NO_TOTALITEMSVIEW;
-			aContinue = false;
-			aError = true;
-		}
+		var rm = aResp.XPath("/s:Envelope/s:Body/m:FindFolderResponse/m:ResponseMessages/m:FindFolderResponseMessage[@ResponseClass='Success' and m:ResponseCode='NoError']");
 
-		if (aContinue) {
+		if (rm.length > 0) {
+
+			var totalItemsInView = rm[0].getAttributeByTag("m:RootFolder", "TotalItemsInView");
+
 			exchWebService.commonFunctions.LOG("totalItemsInView="+totalItemsInView+"\n");
 			if (totalItemsInView == 1) {
+				var folder = rm[0].XPath("/m:RootFolder/t:Folders/*");
 				try {
-					this.argument.folderID = aResp.nsSoap::Body.nsMessages::FindFolderResponse..nsTypes::FolderId.@Id.toString();
-					this.argument.changeKey = aResp.nsSoap::Body.nsMessages::FindFolderResponse..nsTypes::FolderId.@ChangeKey.toString();
-					var folderClass = aResp.nsSoap::Body.nsMessages::FindFolderResponse..nsTypes::FolderClass.toString();
+					this.argument.folderID = folder[0].getAttributeByTag("t:FolderId", "Id");
+					this.argument.changeKey = folder[0].getAttributeByTag("t:FolderId", "ChangeKey");
+					var folderClass = folder[0].getTagValue("t:FolderClass");
 					this.folderCount++;
 					if (this.folderCount < this.folderPath.length) {
 						this.execute();
@@ -157,6 +150,7 @@ erFindFolderRequest.prototype = {
 					}
 				}
 				catch(err) {
+				exchWebService.commonFunctions.LOG("bliepbliep 3");
 					aMsg = err;
 					aCode = this.parent.ER_ERROR_FINDFOLDER_FOLDERID_DETAILS;
 					aContinue = false;
@@ -169,6 +163,12 @@ erFindFolderRequest.prototype = {
 				aCode = this.parent.ER_ERROR_FINDFOLDER_MULTIPLE_RESULTS;
 				aMsg = "Expected on findfolder result but have "+totalItemsInView+" results.";
 			}
+		}
+		else {
+			aMsg = err;
+			aCode = this.parent.ER_ERROR_FINDFOLDER_NO_TOTALITEMSVIEW;
+			aContinue = false;
+			aError = true;
 		}
 
 		if (aError) {
