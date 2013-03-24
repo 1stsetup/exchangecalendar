2f34fd6b33de750969aceaa3f593d1029f50aa9b
Michel <info@1st-setup.nl>
2013-03-24 15:11:22 +0100

    Changed from the less readable, but maybe more protable, defineProperty to __proto__ inheritance
    Signed-off-by: Michel <info@1st-setup.nl>

--- interfaces/exchangeEvent/mivExchangeEvent.js
+++ interfaces/exchangeEvent/mivExchangeEvent.js
@@ -41,106 +41,34 @@ function mivExchangeEvent() {
 
 	this.initExchangeBaseItem();
 
-	Object.defineProperty(this, "startDate", {get : function ()
-		{
-			//this.logInfo("get startdate 1: title:"+this.title);
-			if (!this._startDate) {
-				this._startDate = this.tryToSetDateValue(this.getTagValue("t:Start", null), this._calEvent.startDate);
-				if (this._startDate) {
-					if (this.isAllDayEvent) this._startDate.isDate = true;
-
-					var timezone = this.timeZones.getCalTimeZoneByExchangeTimeZone(this.getTag("t:StartTimeZone"), "");
-					if (timezone) {
-						this._startDate = this._startDate.getInTimezone(timezone);
-					}
-					this._calEvent.startDate = this._startDate.clone();
-				}
-			}
-			//this.logInfo("get startdate 2: title:"+this.title+", startdate=="+this._calEvent.startDate);
-			return this._calEvent.startDate;
-		},
-		                       set : function (aValue)
-		{
-			//this.logInfo("set startdate: title:"+this.title+", aValue:"+aValue);
-			if (aValue.toString() != this.startDate.toString()) {
-				this._newStartDate = aValue;
-				this._calEvent.startDate = aValue;
-			}
-		},
-		                       enumerable : true,
-		                       configurable : true});
-
-
-	Object.defineProperty(this, "endDate", {get : function ()
-		{
-			if (!this._endDate) {
-				this._endDate = this.tryToSetDateValue(this.getTagValue("t:End", null), this._calEvent.endDate);
-				if (this._endDate) {
-					if (this.isAllDayEvent) this._endDate.isDate = true;
-					var timezone = this.timeZones.getCalTimeZoneByExchangeTimeZone(this.getTag("t:EndTimeZone"), "");
-					if (timezone) {
-						this._endDate = this._endDate.getInTimezone(timezone);
-					}
-					this._calEvent.endDate = this._endDate.clone();
-				}
-			}
-			//this.logInfo("get endDate: title:"+this.title+", endDate=="+this._calEvent.endDate, -1);
-			return this._calEvent.endDate;
-		},
-		                       set : function (aValue)
-		{
-			//this.logInfo("set enddate: title:"+this.title+", aValue:"+aValue);
-			if (aValue.toString() != this.endDate.toString()) {
-				this._newEndDate = aValue;
-				this._calEvent.endDate = aValue;
-			}
-		},
-		                       enumerable : true,
-		                       configurable : true});
-
-	Object.defineProperty(this, "duration", {get : function ()
-		{
-			if ((!this._duration) && (!this._newEndDate) && (!this._newStartDate)) {
-				this._duration = this.getTagValue("t:Duration", null);
-				if (this._duration) {
-					//this.logInfo("get duration: title:"+this.title+", value:"+cal.createDuration(this._duration));
-					return cal.createDuration(this._duration);
-				}
-			}
-			//this.logInfo("get duration: title:"+this.title+", value:"+this._calEvent.duration);
-			return this._calEvent.duration;
-		},
-		                       enumerable : true,
-		                       configurable : true});
-
-//		this.logInfo("mivExchangeEvent: init");
+	//this.logInfo("mivExchangeEvent: init");
 
 }
 
 var mivExchangeEventGUID = "4cd0469e-093f-4f7c-8ace-68f6ec76b36e";
 
-mivExchangeEvent.prototype = new mivExchangeBaseItem();
+mivExchangeEvent.prototype = {
 
-mivExchangeEvent.constructor = mivExchangeEvent;
+	__proto__ : mivExchangeBaseItem.prototype,
 
-mivExchangeEvent.prototype.QueryInterface = XPCOMUtils.generateQI([Ci.mivExchangeEvent,
-			Ci.mivExchangeBaseItem,
-			Ci.calIInternalShallowCopy,
-			Ci.calIEvent,
-			Ci.calIItemBase,
-			Ci.nsIClassInfo,
-			Ci.nsISupports]);
+	QueryInterface : XPCOMUtils.generateQI([Ci.mivExchangeEvent,
+				Ci.mivExchangeBaseItem,
+				Ci.calIInternalShallowCopy,
+				Ci.calIEvent,
+				Ci.calIItemBase,
+				Ci.nsIClassInfo,
+				Ci.nsISupports]),
 
-mivExchangeEvent.prototype._className = "mivExchangeEvent";
+	_className : "mivExchangeEvent",
 
-mivExchangeEvent.prototype.classDescription = "Exchange calendar event.";
+	classDescription : "Exchange calendar event.",
 
-mivExchangeEvent.prototype.classID = components.ID("{"+mivExchangeEventGUID+"}");
-mivExchangeEvent.prototype.contractID = "@1st-setup.nl/exchange/calendarevent;1";
-mivExchangeEvent.prototype.flags = Ci.nsIClassInfo.THREADSAFE;
-mivExchangeEvent.prototype.implementationLanguage = Ci.nsIProgrammingLanguage.JAVASCRIPT;
+	classID : components.ID("{"+mivExchangeEventGUID+"}"),
+	contractID : "@1st-setup.nl/exchange/calendarevent;1",
+	flags : Ci.nsIClassInfo.THREADSAFE,
+	implementationLanguage : Ci.nsIProgrammingLanguage.JAVASCRIPT,
 
-mivExchangeEvent.prototype.getInterfaces = function _getInterfaces(count) 
+	getInterfaces : function _getInterfaces(count) 
 	{
 		var ifaces = [Ci.mivExchangeEvent,
 			Ci.mivExchangeBaseItem,
@@ -151,14 +79,82 @@ mivExchangeEvent.prototype.getInterfaces = function _getInterfaces(count)
 			Ci.nsISupports];
 		count.value = ifaces.length;
 		return ifaces;
-	};
+	},
 
-mivExchangeEvent.prototype.initialize = function _initialize()
+	initialize : function _initialize()
 	{
 		this._calEvent = null;
 		this._calEvent = Cc["@mozilla.org/calendar/event;1"]
 					.createInstance(Ci.calIEvent);
-	};
+	},
+
+	get startDate()
+	{
+		//this.logInfo("get startdate 1: title:"+this.title);
+		if (!this._startDate) {
+			this._startDate = this.tryToSetDateValue(this.getTagValue("t:Start", null), this._calEvent.startDate);
+			if (this._startDate) {
+				if (this.isAllDayEvent) this._startDate.isDate = true;
+
+				var timezone = this.timeZones.getCalTimeZoneByExchangeTimeZone(this.getTag("t:StartTimeZone"), "");
+				if (timezone) {
+					this._startDate = this._startDate.getInTimezone(timezone);
+				}
+				this._calEvent.startDate = this._startDate.clone();
+			}
+		}
+		//this.logInfo("get startdate 2: title:"+this.title+", startdate=="+this._calEvent.startDate);
+		return this._calEvent.startDate;
+	},
+
+	set startDate(aValue)
+	{
+		//this.logInfo("set startdate: title:"+this.title+", aValue:"+aValue);
+		if (aValue.toString() != this.startDate.toString()) {
+			this._newStartDate = aValue;
+			this._calEvent.startDate = aValue;
+		}
+	},
+
+	get endDate()
+	{
+		if (!this._endDate) {
+			this._endDate = this.tryToSetDateValue(this.getTagValue("t:End", null), this._calEvent.endDate);
+			if (this._endDate) {
+				if (this.isAllDayEvent) this._endDate.isDate = true;
+				var timezone = this.timeZones.getCalTimeZoneByExchangeTimeZone(this.getTag("t:EndTimeZone"), "");
+				if (timezone) {
+					this._endDate = this._endDate.getInTimezone(timezone);
+				}
+				this._calEvent.endDate = this._endDate.clone();
+			}
+		}
+		//this.logInfo("get endDate: title:"+this.title+", endDate=="+this._calEvent.endDate, -1);
+		return this._calEvent.endDate;
+	},
+
+	set endDate(aValue)
+	{
+		//this.logInfo("set enddate: title:"+this.title+", aValue:"+aValue);
+		if (aValue.toString() != this.endDate.toString()) {
+			this._newEndDate = aValue;
+			this._calEvent.endDate = aValue;
+		}
+	},
+
+	get duration()
+	{
+		if ((!this._duration) && (!this._newEndDate) && (!this._newStartDate)) {
+			this._duration = this.getTagValue("t:Duration", null);
+			if (this._duration) {
+				//this.logInfo("get duration: title:"+this.title+", value:"+cal.createDuration(this._duration));
+				return cal.createDuration(this._duration);
+			}
+		}
+		//this.logInfo("get duration: title:"+this.title+", value:"+this._calEvent.duration);
+		return this._calEvent.duration;
+	},
+};
 
 function NSGetFactory(cid) {
 
