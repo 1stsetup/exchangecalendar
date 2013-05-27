06d1058892b584fbddadf86552c980c7e98ce256
Michel Verbraak <michel@1st-setup.nl>
2013-05-27 13:22:38 +0200

    Fixed bug where folder Properties in Exchange EWS Properties dialog were empty.
    Signed-off-by: Michel Verbraak <michel@1st-setup.nl>

--- chrome/content/exchangeSettings.js
+++ chrome/content/exchangeSettings.js
@@ -209,8 +209,10 @@ exchExchangeSettings.prototype = {
 
 	showFolderProprties: function _showFolderProprties(aProperties)
 	{
+dump("showFolderProprties:"+aProperties.toString()+"\n");
+
 		this.globalFunctions.LOG("showFolderProprties:"+aProperties.toString());
-		var serverVersionInfo = aProperties.XPath('/s:Header/t:ServerVersionInfo')[0];
+		var serverVersionInfo = aProperties.XPath('/s:Header/ServerVersionInfo')[0];
 		this._document.getElementById("exchWebServices-ServerVersionInfo").value = serverVersionInfo.getAttribute('Version') + " ("+serverVersionInfo.getAttribute('MajorVersion')+"."+serverVersionInfo.getAttribute('MinorVersion')+"."+serverVersionInfo.getAttribute('MajorBuildNumber')+"."+serverVersionInfo.getAttribute('MinorBuildNumber')+")";
 
 		var propType = "calendar";
