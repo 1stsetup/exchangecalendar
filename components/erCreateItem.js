a5c6b84e4ed7e41590b3218e2ff86b1fd5ce81ba
Michel Verbraak <info@1st-setup.nl>
2013-12-04 16:05:26 +0100

    Fixes bug 378 New Calender entries always go to main Outlook calendar
    Signed-off-by: Michel Verbraak <info@1st-setup.nl>

--- components/erCreateItem.js
+++ components/erCreateItem.js
@@ -99,9 +99,9 @@ erCreateItemRequest.prototype = {
 		// Following two are only needed for emails.
 		//req.setAttribute("MessageDisposition", "SaveOnly");
 
-		//var savedItemFolderId = makeParentFolderIds2("SavedItemFolderId", this.argument);
-		//req.addChildTagObject(savedItemFolderId);
-		//savedItemFolderId = null;
+		var savedItemFolderId = makeParentFolderIds2("SavedItemFolderId", this.argument);
+		req.addChildTagObject(savedItemFolderId);
+		savedItemFolderId = null;
 
 		var Items = exchWebService.commonFunctions.xmlToJxon('<nsMessages:Items xmlns:nsMessages="'+nsMessagesStr+'" xmlns:nsTypes="'+nsTypesStr+'">'+String(this.createReq)+'</nsMessages:Items>');
 		req.addChildTagObject(Items);
