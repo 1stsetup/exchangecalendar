ab836f966aa9febb8dadff1196a2440426ee174b
Babu Vincent <bavincen.in@gmail.com>
2016-04-26 16:30:03 +0530

    task-fixed

--- chrome/content/exchService-common-sets.js
+++ chrome/content/exchService-common-sets.js
@@ -125,7 +125,7 @@ function newOpenEventDialog(calendarItem, calendar, mode, callback, job, initial
     } else {
         url = "chrome://calendar/content/calendar-summary-dialog.xul";
     }
-    
+    calendarItem = calendarItem.clone();
     if( calendarItem.itemClass == "IPM.Note" )   url = "chrome://exchangecalendar/content/calendar-event-dialog2.xul";
     
     // reminder: event dialog should not be modal (cf bug 122671)
